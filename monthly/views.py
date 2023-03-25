d341c2a51d2a859794f0e19c8499aec68f194a7b
difegam <difegam@gmail.com>
2023-03-25 20:26:14 +0100

    ✨add project base template

--- monthly/views.py
+++ monthly/views.py
@@ -1,7 +1,7 @@
-# from django.shortcuts import render
 import calendar
 
-from django.http import (HttpResponse, HttpResponseNotFound, HttpResponseRedirect)
+from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
 
 challenges = {
@@ -16,11 +16,11 @@ challenges = {
     "september": "Write a program that generates a list of all the prime numbers up to a given number.",
     "october": "Write a program that calculates the factorial of a given number.",
     "november": "Write a program that calculates the sum of the digits of a given number.",
-    "december": "Write a program that converts a given temperature from Celsius to Fahrenheit."
+    "december": "Write a program that converts a given temperature from Celsius to Fahrenheit.",
+    "test": None,
 }
 
 
-# Create your views here.
 def monthly_challenge_by_number(request, month: int):
 
     if month <= 0 or month > len(calendar.month_name):
@@ -28,26 +28,20 @@ def monthly_challenge_by_number(request, month: int):
 
     redirect_path_str = calendar.month_name[month].lower()
     redirect_path = reverse("month-challenge-str", args=[redirect_path_str])
-    print(redirect_path)
-
     return HttpResponseRedirect(redirect_path)
 
 
-# Create your views here.
 def monthly_challenges_list(request):
-    html = ""
-    for month in challenges:
-        url_path = reverse("month-challenge-str", args=[month])
-        html += f'<li><a href="{url_path}">{month.capitalize()}</a></li>'
-    reponse_data = f"<ol>{html}</ol>"
-    return HttpResponse(reponse_data)
+    months = challenges.keys()
+    return render(request, "monthly/index.html", context={"months": months})
 
 
-def monthly_challenge(request, month):
-
+def monthly_challenge(request, month: str):
     challenge = challenges.get(month.lower())
 
     if not challenge:
         return HttpResponseNotFound(f"Please enter a valid month. {month} in not supported")
 
-    return HttpResponse(f"The challenge for {month.capitalize()} is: {challenge}")
+    context = {"month": month, "challenge": challenge}
+
+    return render(request, "monthly/monthly_challenge.html", context=context)
